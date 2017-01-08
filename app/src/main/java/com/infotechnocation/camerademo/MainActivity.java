eeefff2d700a6c929d4904c3eedb6a68250be8bb
theprajin <prazenthekiller@gmail.com>
2017-01-08 17:21:46 +0545

    initial commit

---
+++ app/src/main/java/com/infotechnocation/camerademo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.infotechnocation.camerademo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnStartCamera;
+    ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStartCamera = (Button)findViewById(R.id.btnStartCamera);
+        mImageView = (ImageView)findViewById(R.id.img);
+
+        btnStartCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        checkPermission();
+
+    }
+
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},10);
+        }else{
+            openCamera();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 10 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            openCamera();
+
+        }
+
+    }
+
+    private void openCamera(){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        //startActivity(intent);
+
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == 100){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
